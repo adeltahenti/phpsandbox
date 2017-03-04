f51f3112d60c8b03c64efc51e91a9bb41618dd09
Adel <Adel>
2017-03-04 10:12:34 +0100

    Des infos

--- variables.php
+++ variables.php
@@ -7,6 +7,13 @@
       Comment
     */
 
+    # Variables
+    /*
+      - Prefix $
+      - Start with a letter or an underscore
+      - Only letters , numbers and underscore
+      - Case sensitive
+    */
     $output = "Hello world!";
     echo $output;
 
